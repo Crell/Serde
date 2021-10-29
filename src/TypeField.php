707f3b5221f859dfa81fb8360bf22cad3bea82bc
Larry Garfield <larry@garfieldtech.com>
2021-10-29 12:52:00 -0500

    Doc improvement.

--- src/TypeField.php
+++ src/TypeField.php
@@ -6,5 +6,8 @@ namespace Crell\Serde;
 
 interface TypeField
 {
+    /**
+     * Determines if this field is valid for a given type.
+     */
     public function acceptsType(string $type): bool;
 }
