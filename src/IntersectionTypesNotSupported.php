1e4071c74d45f82ef5309d108e48786dc6c2a98f
Larry Garfield <larry@garfieldtech.com>
2022-07-22 11:16:54 -0500

    Improve exception error messages for unsupported compound types.

--- src/IntersectionTypesNotSupported.php
+++ src/IntersectionTypesNotSupported.php
@@ -6,13 +6,15 @@ namespace Crell\Serde;
 
 class IntersectionTypesNotSupported extends \TypeError
 {
-    // @todo Use this in the error message.
     public readonly \ReflectionProperty $property;
 
     public static function create(\ReflectionProperty $property): self
     {
         $new = new self();
         $new->property = $property;
+
+        $new->message = sprintf('Serde does not currently support intersection type properties. The property %s has a union type, and thus cannot be serialized/deserialized.', $property->name);
+
         return $new;
     }
 }
--- src/UnionTypesNotSupported.php
+++ src/UnionTypesNotSupported.php
@@ -4,15 +4,19 @@ declare(strict_types=1);
 
 namespace Crell\Serde;
 
+use Crell\Serde\Attributes\Field;
+
 class UnionTypesNotSupported extends \TypeError
 {
-    // @todo Use this in the error message.
     public readonly \ReflectionProperty $property;
 
     public static function create(\ReflectionProperty $property): self
     {
         $new = new self();
         $new->property = $property;
+
+        $new->message = sprintf('Serde does not currently support union type properties. The property %s has a union type, and thus cannot be serialized/deserialized.', $property->name);
+
         return $new;
     }
 }
