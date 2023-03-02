8ea968d5cc34d6ea26ec65ad1beb1a4eb8c06098
Larry Garfield <larry@garfieldtech.com>
2023-03-01 19:58:40 -0600

    Leave a note about adding deserializing format restrictions to datetime in the future.

--- src/PropertyHandler/DateTimeExporter.php
+++ src/PropertyHandler/DateTimeExporter.php
@@ -54,11 +54,21 @@ class DateTimeExporter implements Exporter, Importer
             return null;
         }
 
+        // @todo It would be helpful to use https://www.php.net/manual/en/datetimeimmutable.createfromformat.php
+        // to only accept value sin the expected format. However, that method
+        // auto-fills missing values with the current time.  The constructor does
+        // not.  So if you have a Y-m-d string of 2022-07-04T00:00:00 and create a new DTI
+        // using the constructor, you get that timestamp. If you use createFromFormat(),
+        // you get that date but the time is filled in with whenever you ran the code.
+        // Once we figure out how to deal with that (arguably it's a PHP bug),
+        // an opt-in/out flag to restrict the format on import should be added
+        // to DateField to get used here.  Until then, this will do.
+
         return new ($field->phpType)($string);
     }
 
     public function canImport(Field $field, string $format): bool
     {
-        return in_array($field->phpType, [\DateTimeInterface::class, \DateTime::class, \DateTimeImmutable::class]);
+        return in_array($field->phpType, [\DateTimeInterface::class, \DateTime::class, \DateTimeImmutable::class], true);
     }
 }
