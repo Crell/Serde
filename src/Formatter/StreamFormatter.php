be204ec60d98a5a3dc9999a9910832dedb987301
Larry Garfield <larry@garfieldtech.com>
2023-03-01 19:05:20 -0600

    Add a CSV Stream-based formatter.

---
+++ src/Formatter/StreamFormatter.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Crell\Serde\Formatter;
+
+use Crell\Serde\Attributes\ClassSettings;
+use Crell\Serde\Attributes\Field;
+use Crell\Serde\Serializer;
+
+trait StreamFormatter
+{
+
+    public function rootField(Serializer $serializer, string $type): Field
+    {
+        return Field::create('root', $type);
+    }
+
+    public function serializeInitialize(ClassSettings $classDef, Field $rootField): mixed
+    {
+        return FormatterStream::new(fopen('php://temp/', 'wb'));
+    }
+
+    public function serializeFinalize(mixed $runningValue, ClassSettings $classDef): mixed
+    {
+        return $runningValue;
+    }
+
+    public function serializeInt(mixed $runningValue, Field $field, int $next): mixed
+    {
+        $runningValue->write((string)$next);
+        return $runningValue;
+    }
+
+    public function serializeFloat(mixed $runningValue, Field $field, float $next): mixed
+    {
+        $runningValue->write((string)$next);
+        return $runningValue;
+    }
+
+    public function serializeString(mixed $runningValue, Field $field, string $next): mixed
+    {
+        $runningValue->printf('"%s"', $next);
+        return $runningValue;
+    }
+
+    public function serializeBool(mixed $runningValue, Field $field, bool $next): mixed
+    {
+        $runningValue->write($next ? 'true' : 'false');
+        return $runningValue;
+    }
+}
---
+++ tests/CsvStreamFormatterTest.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Crell\Serde;
+
+use Crell\Serde\Formatter\CsvFormatter;
+use Crell\Serde\Formatter\CsvStreamFormatter;
+use Crell\Serde\Formatter\FormatterStream;
+use Crell\Serde\Records\CsvRow;
+use Crell\Serde\Records\CsvTable;
+use Crell\Serde\Records\CsvTableLazy;
+use Crell\Serde\Records\Point;
+use Crell\Serde\Records\PointList;
+use PHPUnit\Framework\TestCase;
+
+class CsvStreamFormatterTest extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider csvExamples()
+     */
+    public function csv_serialize(object $data, ?object $expected = null): void
+    {
+        $s = new SerdeCommon(formatters: [new CsvStreamFormatter(), new CsvFormatter()]);
+
+        // Use a temp stream as a placeholder.
+        $init = FormatterStream::new(fopen('php://temp/', 'wb'));
+
+        $result = $s->serialize($data, format: 'csv-stream', init: $init);
+
+        fseek($result->stream, 0);
+        $csv = stream_get_contents($result->stream);
+
+        $expected ??= $data;
+
+        $deserialized = $s->deserialize($csv, from: 'csv', to: $data::class);
+
+        self::assertEquals($expected, $deserialized);
+    }
+
+    public static function csvExamples(): iterable
+    {
+        yield [
+            new PointList([
+                new Point(1, 2, 3),
+                new Point(4, 5, 6),
+                new Point(7, 8, 9),
+            ]),
+        ];
+
+        yield [
+            new CsvTable([
+                new CsvRow('Larry', 100, 500),
+                new CsvRow('Curly', 25, 25.25),
+                new CsvRow('Moe', 31, 99.9999),
+            ]),
+        ];
+
+        $rows = static function() {
+            yield new CsvRow('Larry', 100, 500);
+            yield new CsvRow('Curly', 25, 25.25);
+            yield new CsvRow('Moe', 31, 99.9999);
+        };
+
+        yield [
+            'data' => new CsvTableLazy($rows()),
+            'expected' => new CsvTableLazy([
+                new CsvRow('Larry', 100, 500),
+                new CsvRow('Curly', 25, 25.25),
+                new CsvRow('Moe', 31, 99.9999),
+            ]),
+        ];
+    }
+}
