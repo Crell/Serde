bff7607f7afb9476173a9c1813ffe55dd7abe73b
John Boctor <jboctor@outlook.com>
2024-05-20 12:54:14 -0500

    Update TypeField.php

--- src/TypeField.php
+++ src/TypeField.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Crell\Serde;
 
-interface TypeField
+use Crell\AttributeUtils\Inheritable;
+use Crell\AttributeUtils\TransitiveProperty;
+
+interface TypeField extends Inheritable, TransitiveProperty
 {
     /**
      * Determines if this field is valid for a given type.
