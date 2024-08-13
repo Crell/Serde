e94d64065582e2adb763bf23bfcf2f953364f585
Vano Devium <webdevium@gmail.com>
2024-08-13 20:32:03 +0300

    TOML support

--- tests/TomlFormatterTest.php
+++ tests/TomlFormatterTest.php
@@ -15,6 +15,11 @@ class TomlFormatterTest extends ArrayBasedFormatterTestCases
     public function setUp(): void
     {
         parent::setUp();
+
+        if (version_compare(PHP_VERSION, '8.2') < 0) {
+            $this->markTestSkipped('PHP >=8.2 is required to run this test');
+        }
+
         $this->formatters = [new TomlFormatter()];
         $this->format = 'toml';
         $this->emptyData = '';
